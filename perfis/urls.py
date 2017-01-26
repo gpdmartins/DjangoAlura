1cb6cd5be179bac68350205a11c12b2a9894da1d
Jose Guilherme Martins <gpdmartins@gmail.com>
2017-01-25 22:54:43 -0200

    adicionado numero de perfis., ex3'

--- perfis/urls.py
+++ perfis/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import patterns, url
 from perfis.views import index, exibir
 
-#urlpatterns = patterns('',
- #   url(r'^$', 'perfis.views.index')
-#)   
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^perfil$', exibir, name='exibir')
+    url(r'^perfis/(?P<perfil_id>\d+)$', exibir, name='exibir'),
 ]
--- perfis/views.py
+++ perfis/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from perfis.models import Perfil
 
 
 def index(request):
     return render(request, 'index.html')
 
-def exibir(request):
-    return render(request, 'perfil.html')
+def exibir(request, perfil_id):
+    perfil = Perfil()
+    if perfil_id == '1':
+        perfil = Perfil('Guilherme Martins', 'guilherme@email.com',  '123456', 'ONU')
+    return render(request, 'perfil.html', {"perfil": perfil})
