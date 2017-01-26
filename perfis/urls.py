6962c610d0d57424ba866c2bf9e1eee9cb0cae11
Jose Guilherme Martins <gpdmartins@gmail.com>
2017-01-25 22:03:36 -0200

    adicionando perfil

--- perfis/urls.py
+++ perfis/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import patterns, url
+from perfis.views import index, exibir
 
-urlpatterns = patterns('',
-    url(r'^$', 'perfis.views.index')
-)   
+#urlpatterns = patterns('',
+ #   url(r'^$', 'perfis.views.index')
+#)   
+urlpatterns = [
+    url(r'^$', index, name='index'),
+    url(r'^perfil$', exibir, name='exibir')
+]
